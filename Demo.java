7e662a20477a0ce9c1c261fdace18cf822424fe3
26adi <aditya26.bhl@gmail.com>
2024-03-07 11:02:04 +0530

    add commit

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.printf("Hello");
         System.out.printf("Hi");
+        System.out.println("gogo");
     }
 }
